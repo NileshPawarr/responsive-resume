5abc891687a2724f46b31950e39e3d292c6fddee
Nilesh Pawar <nileshpawar9922@gmail.com>
2021-06-26 13:56:39 +0530

    fix scroll top button issue

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -105,7 +105,7 @@ window.addEventListener('scroll', scrollActive);
 const scrollTop = () => {
     const scrollTop = document.getElementById('scroll-top');
     /* When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class */
-    (this.scrollY >= 200) ? scrollTop.classList.add('show-scroll') : scrollTop.classList.remove('show-scroll');
+    (window.scrollY >= 200) ? scrollTop.classList.add('show-scroll') : scrollTop.classList.remove('show-scroll');
 };
 window.addEventListener('scroll', scrollTop);
 
